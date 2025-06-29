bb8dfeeda3efa13469d2ee277b96458ebfe35c14
Sneha P Pratap <snehapratap248@gmail.com>
2025-06-29 12:02:40 +0530

    frontend setup done

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import InputForm from "./components/InputForm";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>AI Reading Comprehension Generator</h2>
+      <InputForm />
+    </div>
+  );
+}
+
+export default App;
