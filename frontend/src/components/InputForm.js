512c3695968e6a9244545f6a70947be0f73ee23e
Sneha P Pratap <snehapratap248@gmail.com>
2025-06-29 12:05:36 +0530

    done

---
+++ frontend/src/components/InputForm.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function InputForm() {
+  const [passage, setPassage] = useState("");
+  const [grade, setGrade] = useState("5");
+  const [response, setResponse] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await axios.post("http://localhost:8000/generate", {
+      passage,
+      grade_level: parseInt(grade),
+    });
+    setResponse(res.data.questions);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <textarea rows="8" cols="60" value={passage} onChange={e => setPassage(e.target.value)} placeholder="Paste your passage here" />
+        <br />
+        <label>
+          Grade Level:
+          <select value={grade} onChange={e => setGrade(e.target.value)}>
+            {[...Array(12).keys()].map(i => <option key={i+1}>{i+1}</option>)}
+          </select>
+        </label>
+        <br />
+        <button type="submit">Generate Questions</button>
+      </form>
+      {response && (
+        <div>
+          <h3>Generated Questions & Answers</h3>
+          <pre>{response}</pre>
+        </div>
+      )}
+    </div>
+  );
+}
